281faaf5031854f6b0b3aa6a553a6c2c51a1dce1
defendi.andrea <defendi.andrea@BIBLIO-PC03.OSDBSESTO.local>
2020-01-27 12:56:54 +0100

    start

---
+++ BufferedReader_VS_Scanner/src/bufferedreader_vs_scanner/BufferedReader_VS_Scanner.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bufferedreader_vs_scanner;
+
+/**
+ *
+ * @author defendi.andrea
+ */
+public class BufferedReader_VS_Scanner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
